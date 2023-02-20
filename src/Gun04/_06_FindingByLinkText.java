2273bcac378f92046a710cee26da71a89d5fefd4
Metin Aykul <metinaykul1@gmail.com>
2023-02-20 22:04:59 +0300

    LinkText ile eleman bulundu a href olan linkler alındı

--- src/Gun04/_06_FindingByLinkText.java
+++ src/Gun04/_06_FindingByLinkText.java
@@ -1,4 +1,22 @@
 package Gun04;
 
+import Utility.MyFunction;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class _06_FindingByLinkText {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.hepsiburada.com");
+        WebElement siparislerimLinki= driver.findElement(By.linkText("Siparişlerim"));
+        // Gözüken texti Siparişlerim olan a taglı webelement
+        System.out.println("siparislerimLinki.getText() = " + siparislerimLinki.getText());
+
+        MyFunction.Bekle(3);
+        driver.quit();
+
+    }
 }
