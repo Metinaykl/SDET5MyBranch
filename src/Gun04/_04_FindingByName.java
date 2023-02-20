e96c056851e7b345e4bf06b9117dee92c6e207c9
Metin Aykul <metinaykul1@gmail.com>
2023-02-20 21:16:23 +0300

    name ile eleman bulmayı yaptık

---
+++ src/Gun04/_04_FindingByName.java
@@ -0,0 +1,21 @@
+package Gun04;
+
+import Utility.MyFunction;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _04_FindingByName {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://form.jotform.com/221934510376353");
+      WebElement ad= driver.findElement(By.name("q8_name[first]"));
+      ad.sendKeys("Metin");
+
+        MyFunction.Bekle(3);
+        driver.quit();
+
+    }
+}
