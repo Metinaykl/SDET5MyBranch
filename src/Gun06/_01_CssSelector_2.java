45e97b677ab143564fab4a24880634dba3be1627
Metin Aykul <metinaykul1@gmail.com>
2023-02-23 02:40:52 +0300

    ilk tam anlamıyla test gercekleştirildi.

--- src/Gun06/_01_CssSelector_2.java
+++ src/Gun06/_01_CssSelector_2.java
@@ -1,24 +1,27 @@
 package Gun06;
 import Utility.BaseDriver;
 import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class _01_CssSelector_2 extends BaseDriver {
-    public static void main(String[] args)  {
+    @Test
+    public void Test1()  {
 
         driver.get("https://demo.seleniumeasy.com/basic-first-form-demo.html");
+        String mesajim="Merhaba ben metin";
 
         WebElement txtbox= driver.findElement(By.cssSelector("input[id='user-message']"));
-        txtbox.sendKeys("Merhaba ben metin");
+        txtbox.sendKeys(mesajim);
         WebElement seemesaj=driver.findElement(By.cssSelector("[onclick='showInput();']"));
         seemesaj.click();
         WebElement msg=driver.findElement(By.cssSelector("[id='display']"));
 
-        if (msg.getText().equals("Merhaba ben metin"))
-            System.out.println("test passed");
-        else
-            System.out.println("test fail");
+        Assert.assertTrue("aranilan mesaj bulunamadı",msg.getText().equals(mesajim)); // beklenen expect
+        // assertTrue: içindeki değer True mu ? True ise hiç bir şey yapmaz.mesaj vermez
+        // değilse beklenen olmamışsa hata mesajı verir.
+        //yani genel kural : Hata var ise göster
 
         BekleKapat();
 
